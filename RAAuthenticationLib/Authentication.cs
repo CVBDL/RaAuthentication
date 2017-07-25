8d8c1a2a614b89553e3e72df3ce953b12eca269f
HHDong1988 <donghuanhuan1988@hotmail.com>
2017-07-25 09:38:01 +0800

    add authenticationlib

---
+++ RAAuthenticationLib/Authentication.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace RAAuthenticationLib
+{
+    public class Authentication
+    {
+        static public bool CheckAuthenticate(string userName, string password,string domainDame)
+        {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainDame, userName, password))
+            {
+                return pc.ValidateCredentials(userName, password, ContextOptions.Negotiate);
+            }     
+        }
+
+        static public UserDetail GetUserEmailFromAD(string userName, string password, string domainName)
+        {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, domainName, userName, password))
+            {
+                UserPrincipal user = UserPrincipal.FindByIdentity(pc, userName);
+                UserDetail detail = new UserDetail
+                {
+                    DisplayName = user.DisplayName,
+                    EmailAddress = user.EmailAddress,
+                    EmployeeID = user.EmployeeId,
+                    Name = user.Name
+                };
+                return detail;
+            }
+        }
+    }
+}
---
+++ RAAuthenticationLib/UserDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAAuthenticationLib
+{
+    public class UserDetail
+    {
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string EmployeeID { get; set; }
+        public string Name { get; set; }
+    }
+}
