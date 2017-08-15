0673f5d1724fdf6077bf6088acf4b3971b28d718
zhongsp <patrick.zhongsp@gmail.com>
2017-08-15 16:21:21 +0800

    list endpoint categories

---
+++ RAAuthentication/Controllers/RootEndpointController.cs
@@ -0,0 +1,18 @@
+﻿using RAAuthentication.Models;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace RAAuthentication.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api")]
+    public class RootEndpointController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult ListEndpointCategories()
+        {
+            return Ok(new EndpointCategoriesDTO());
+        }
+    }
+}
---
+++ RAAuthentication/Models/EndpointCategoriesDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+
+namespace RAAuthentication.Models
+{
+    public class EndpointCategoriesDTO
+    {
+        private string rootEndpoint = ConfigurationManager.AppSettings.Get("RootEndpoint");
+
+        public string UserAuthenticationUrl
+        {
+            get
+            {
+                return rootEndpoint + "/api/user{?scope}";
+            }
+        }
+
+        public string UserDetailsUrl
+        {
+            get
+            {
+                return rootEndpoint + "/api/user/details";
+            }
+        }
+
+        public string TokenValidationUrl
+        {
+            get
+            {
+                return rootEndpoint + "/api/token/validate";
+            }
+        }
+    }
+}
